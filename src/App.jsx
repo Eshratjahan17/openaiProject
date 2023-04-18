7fc7d996445261fe26537324b247154c598eb383
Eshrat <hasan.techtomorrow@gmail.com>
2023-04-18 11:40:45 +0600

    API key error unresolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+const API_KEY = "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";
 
 function App() {
+ 
   const [tweet, setTweet] = useState("");
-  const API_KEY = "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";
+  const [Sentiment, setSentiment] = useState("");
   console.log(tweet);
-  const callOpenAIAPI = () => {
+  const callOpenAIAPI = async () => {
     console.log("open ai calling");
-}
+    const APIBody = {
+      model: "text-davinci-003",
+      prompt: "what is the sentiment of this tweet?" + tweet,
+      temperature: 0,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    };
+    await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer" + API_KEY,
+      },
+     
+      body: JSON.stringify(APIBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data,API_KEY);
+      });
+  };
   return (
-    <div className='text-area'>
+    <div className="text-area">
       <textarea
         onChange={(e) => setTweet(e.target.value)}
-        placeholder='Your tweet here'
+        placeholder="Your tweet here"
         cols={50}
         rows={10}
-      >
-        
-      </textarea>
+      ></textarea>
       <button onClick={callOpenAIAPI}>Test tweet Sentiment</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
